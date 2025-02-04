c8c58ebbe296f85a396b5cbeb063115bf0cdcbdc
komatr <rudolf.bouzek@seznam.cz>
2025-02-04 20:42:58 +0100

    tile test tiles

---
+++ tiled/tilesets/grassland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="grassland" tilewidth="64" tileheight="32" tilecount="672" columns="16">
+ <image source="../../resources/tilesets/grassland.png" width="1024" height="1344"/>
+</tileset>
