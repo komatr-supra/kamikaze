e35a03e227a914029800feb934e89dcf21da96ea
komatr <rudolf.bouzek@seznam.cz>
2025-02-06 04:33:01 +0100

    new map data layout
    tiled in root folder is not used by game, its tiled data and images for texturepacker, generated files from texturepacker are added to src/resources/tilesets/*tileset_name*/ this also contain tilesheet and information about sprite datas inside texture

---
+++ src/resources/tilesets/greenland/cliffs.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="cliffs" tilewidth="64" tileheight="96" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff1.png" width="64" height="96"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff5.png" width="64" height="96"/>
+ </tile>
+ <tile id="0">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff0.png" width="64" height="96"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff4.png" width="64" height="96"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff3.png" width="64" height="96"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff7.png" width="64" height="96"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff2.png" width="64" height="96"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff6.png" width="64" height="96"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff9.png" width="64" height="96"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff13.png" width="64" height="96"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff8.png" width="64" height="96"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff12.png" width="64" height="96"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff15.png" width="64" height="96"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff11.png" width="64" height="96"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff10.png" width="64" height="96"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff14.png" width="64" height="96"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff17.png" width="64" height="96"/>
+ </tile>
+ <tile id="21">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff21.png" width="64" height="96"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff16.png" width="64" height="96"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff20.png" width="64" height="96"/>
+ </tile>
+ <tile id="23">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff23.png" width="64" height="96"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff19.png" width="64" height="96"/>
+ </tile>
+ <tile id="22">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff22.png" width="64" height="96"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../../tiled/tilesets/greenland/cliffs/cliff18.png" width="64" height="96"/>
+ </tile>
+</tileset>
---
+++ src/resources/tilesets/greenland/trees.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="trees" tilewidth="128" tileheight="224" tilecount="16" columns="0">
+ <tileoffset x="-32" y="0"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree0.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree2.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree3.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree4.png" width="128" height="192"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree5.png" width="128" height="224"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree6.png" width="128" height="224"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree7.png" width="128" height="224"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree8.png" width="128" height="192"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree9.png" width="128" height="192"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree10.png" width="128" height="192"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree11.png" width="128" height="192"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree12.png" width="128" height="160"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree13.png" width="128" height="160"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree14.png" width="128" height="160"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../../tiled/tilesets/greenland/trees/tree15.png" width="128" height="160"/>
+ </tile>
+</tileset>
