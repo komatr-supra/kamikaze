61c457b760837c912a8b5bf93d0f334fb0ff3227
Maicon Santana <maiconspas@gmail.com>
2025-02-10 17:06:48 +0000

    Collecting all map values but image source for now, freeing most of the pointers

--- src/include/map_manager.h
+++ src/include/map_manager.h
@@ -6,16 +6,28 @@
 #include <string.h>
 #include <ctype.h>
 // #include <dirent.h> 
+#include <limits.h>
+#include <errno.h>
 
 #define MAX_TILESETS 5 // Adjust as needed
+#define MAX_TILESET_TILES 30 // Adjust as needed
 #define MAX_LAYERS 10 // Adjust as needed
 #define MAX_TILE_DATA_SIZE 1024 // Adjust as needed
 #define MAX_NAME_LENGTH 20 // Adjust as needed
 #define MAX_SOURCE_LENGTH 64 // Adjust as needed
 
+typedef struct TilesetTile {
+    char* source;
+    int id;
+} TilesetTile;
+
 typedef struct Tileset {
-    char source[MAX_SOURCE_LENGTH];
+    TilesetTile* tilesetTile;
+    char* source;
+    char name[MAX_NAME_LENGTH];
     int firstgid;
+    int numTilesetTiles;
+    int tilecount;
 } Tileset;
 
 typedef struct Layer {
@@ -27,7 +39,7 @@ typedef struct Layer {
 } Layer;
 
 typedef struct Map {
-    Tileset tilesets[MAX_TILESETS];
+    Tileset* tilesets;
     Layer layers[MAX_LAYERS];
     char version[10];
     char renderorder[20];
@@ -40,15 +52,97 @@ typedef struct Map {
 } Map;
 /*
 * May change until the final version
-* Size of Map: 592 bytes
-* Size of Layer: 40 bytes
-* Size of Tileset: 68 bytes
+* Map length 544
+* Tileset length 48
+* Layer length 48
+* TilesetTile length 16
 **/
 
 void SkipWhitespace(char **xml) {
     while (isspace((unsigned char)**xml)) {
         (*xml)++;
     }
+}
+
+char* ResourcePath(char *path) {
+    if (path == NULL) {
+        return NULL;
+    }
+
+    const char* replacement = "./resources";
+    const char* target = "..";
+    size_t repl_len = strlen(replacement);
+    size_t target_len = strlen(target);
+
+    char* result = (char *)malloc(strlen(path) * 2 + 1); // Allocate enough space. (+1 for null terminator)
+    if(result == NULL)
+    {
+        perror("malloc failed");
+        return NULL;
+    }
+
+    char* current_pos = path;
+    char* write_pos = result;
+
+    size_t prefix_len = 0;
+    while ((current_pos = strstr(current_pos, target)) != NULL) {
+        // Copy everything up to the ".."
+        prefix_len = (current_pos - path)+2;
+        strncpy(write_pos, path, prefix_len);
+        write_pos += prefix_len;
+
+        // Copy the replacement string
+        strncpy(write_pos, replacement, repl_len);
+        write_pos += repl_len;
+
+        // Move past the ".." in the original string
+        current_pos += target_len;
+        path = current_pos; // Important: Update path_copy
+    }
+
+    // Copy the remaining part of the string (after the last "..")
+    strcpy(write_pos, path);
+    result[prefix_len] = '\0';
+
+    return result;
+}
+
+bool TilesetParser(Tileset* tileset){
+    // Load Tileset
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    FILE* file = fopen(tileset->source, "r");
+    if (file == NULL){
+        TraceLog(LOG_ERROR, "File not loaded");
+        return false;
+    }
+
+    long fileSize = ftell(file);
+    rewind(file);
+    tileset->numTilesetTiles = 0;
+    size_t currentTileIndex = 0;
+    bool isCollectingTile = false;
+    tileset->tilecount = 0;
+    while ((read = getline(&line, &len, file)) != -1) {
+        SkipWhitespace(&line);
+        if (isCollectingTile){
+
+            isCollectingTile = false;
+            currentTileIndex++;
+        }else if(strstr(line, "<tileset ")){
+            char* tilecountStr = strstr(line, "tilecount=\"");
+            if (tilecountStr) tileset->tilecount = atoi(tilecountStr + 11);
+            tileset->tilesetTile = malloc(sizeof(TilesetTile) * tileset->tilecount);
+        }else if (strstr(line, "<tile ") && tileset->tilecount != 0) {
+            char* idStr = strstr(line, "id=\"");
+            if (idStr) tileset->tilesetTile[currentTileIndex].id = atoi(idStr + 4);
+            isCollectingTile = true;
+        } 
+    }
+
+    return true;
 }
 
 // Function to read the XML file into a string
@@ -80,22 +174,34 @@ bool ReadFile(const char filename[], Map* map) {
     map->numLayers = 0;
     bool isCollectingLayerData = false;
     int collectingLayoutDataIndex = 0;
+    size_t maxCurrentDataSize = 0;
+    size_t currentDataLen = 0; // Start at the beginning
+    map->tilesets = malloc(sizeof(Tileset) * MAX_TILESETS);
     while ((read = getline(&line, &len, file)) != -1) {
         SkipWhitespace(&line);
         // TraceLog(LOG_INFO, TextFormat("Retrieved line of length %zu:", read));
         // TraceLog(LOG_INFO, TextFormat("%s", line));
         // Parse line by line
         if (isCollectingLayerData){
+            // This does not work yet
+            int currentLayer = map->numLayers-1;
             if (*line == '<') {
                 isCollectingLayerData = false;
             } else {
-                // This does not work yet
-                int currentLayer = map->numLayers-1;
-                if(map->layers[currentLayer].data){
-                    for(int i=0;i< map->layers[currentLayer].width;i++){
-                        if(isdigit(*(line + i))){
-                            map->layers[currentLayer].data[i] = atoi(line+i);
+                if(map->layers[currentLayer].data){ 
+                    char delimiters[1] = ",";
+                    char* token = strtok(line, delimiters);
+                    
+                    while (token != NULL) {
+                        char *endptr;
+                        errno = 0;
+                        int num = strtol(token, &endptr, 10);
+                        if (*endptr != '\n' && errno == 0 && num >= INT_MIN && num <= INT_MAX) {
+                            map->layers[currentLayer].data[currentDataLen] = num;
+                            currentDataLen++;
                         }
+
+                        token = strtok(NULL, delimiters); // I Dont get it but they say to use it to loop
                     }
                 }
             }
@@ -116,7 +222,7 @@ bool ReadFile(const char filename[], Map* map) {
             if (map->numTilesets < MAX_TILESETS) {
                 char* sourceStr = strstr(line, "source=\"");
                 if (sourceStr) {
-                    sourceStr += 6;
+                    sourceStr += 8;
                     char* sourceEndStr = strchr(sourceStr, '"');
                     if (sourceEndStr) {
                         size_t sourceLen = sourceEndStr - sourceStr;
@@ -124,12 +230,15 @@ bool ReadFile(const char filename[], Map* map) {
                             fclose(file);
                             return false;
                         }
+                        map->tilesets[map->numTilesets].source = malloc(sourceLen+2);
                         strncpy(map->tilesets[map->numTilesets].source, sourceStr, sourceLen);
+                        map->tilesets[map->numTilesets].source[sourceLen] = '\0';
+                        map->tilesets[map->numTilesets].source = ResourcePath(map->tilesets[map->numTilesets].source);
                     }
-                    strcpy(map->tilesets[map->numTilesets].source, sourceStr + 8);
                 }
                 char* firstgidStr = strstr(line, "firstgid=\"");
                 if (firstgidStr) map->tilesets[map->numTilesets].firstgid = atoi(firstgidStr + 10);
+                TilesetParser(&map->tilesets[map->numTilesets]);
                 map->numTilesets++;
             }
         } else if (strstr(line, "<layer")) {
@@ -156,7 +265,9 @@ bool ReadFile(const char filename[], Map* map) {
                 char* heightStr = strstr(line, "height=\"");
                 if (heightStr) map->layers[map->numLayers].height = atoi(heightStr + 8);
                 // Alloc array by layer size
-                map->layers[map->numLayers].data = (int*)malloc(map->layers[map->numLayers].width * map->layers[map->numLayers].height * sizeof(int));
+                maxCurrentDataSize = map->layers[map->numLayers].width * map->layers[map->numLayers].height * sizeof(int);
+                map->layers[map->numLayers].data = NULL;
+                map->layers[map->numLayers].data = (int*)malloc(maxCurrentDataSize);
                 if (map->layers[map->numLayers].data == NULL) {
                     TraceLog(LOG_ERROR, "Memory allocation failed for layer data!");
                     fclose(file);
@@ -167,10 +278,15 @@ bool ReadFile(const char filename[], Map* map) {
         } else if (strstr(line, "<data")) {
             isCollectingLayerData = true;
             collectingLayoutDataIndex = 0;
+            currentDataLen = 0;
         }
     }
 
     fclose(file);
+    TraceLog(LOG_INFO, TextFormat("Map length %d", sizeof(Map)));
+    TraceLog(LOG_INFO, TextFormat("Tileset length %d", sizeof(Tileset)));
+    TraceLog(LOG_INFO, TextFormat("Layer length %d", sizeof(Layer)));
+    TraceLog(LOG_INFO, TextFormat("TilesetTile length %d", sizeof(TilesetTile)));
 
     return true;
 }
@@ -180,6 +296,19 @@ void MapFree(Map* map){
             if(map->layers[i].data){
                 free(map->layers[i].data);
             }
+        }
+        for(int i=0;i<map->numTilesets;i++){
+            if(map->tilesets[i].source){
+                free(map->tilesets[i].source);
+            }
+            for(int j=0;j<map->tilesets[i].numTilesetTiles;j++){
+                if(map->tilesets[i].tilesetTile[j].source){
+                    free(map->tilesets[i].tilesetTile[j].source);
+                }
+            }
+        }
+        if(map->tilesets){
+            free(map->tilesets);
         }
         free(map);
     }
