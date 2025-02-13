237c1d5479fb4597f6a476cf6f8ea4216dfdbd08
Maicon Santana <maiconspas@gmail.com>
2025-02-13 22:55:04 +0000

    Split main in 2 and add endmode2d

---
+++ src/include/komatr_main.h
@@ -0,0 +1,22 @@
+// File to be deleted in the future (doing some speed coding)
+#ifndef KOMATR_MAIN_H
+#define KOMATR_MAIN_H
+#include "animation.h"
+
+void KomatrInit(){
+    
+}
+
+void KomatrUpdate(float delta_time){
+
+}
+
+void KomatrDraw(){
+    
+}
+
+void KomatrUnload(){
+
+}
+
+#endif //KOMATR_MAIN_H
--- src/main.c
+++ src/main.c
@@ -2,8 +2,17 @@
 #include <stdio.h>
 
 #include "raylib.h"
-#include "map_manager.h"
-#include "animation.h"
+
+// Uncoment these lines to be able to test code separeted (Remove this speed coding when it is time)
+// #define MAICON_TESTING 1 
+// #define KOMATR_TESTING 1
+
+#ifdef MAICON_TESTING
+#include "maicon_main.h"
+#endif
+#ifdef KOMATR_TESTING
+#include "komatr_main.h"
+#endif
 
 int main(void)
 {
@@ -19,18 +28,25 @@ int main(void)
     camera.target = (Vector2){300, 300};
     camera.zoom = 2.0f;
 
+#ifdef MAICON_TESTING
+    MaiconInit();
+#endif
+#ifdef KOMATR_TESTING
+    KomatrInit();
+#endif
     Shader blurrShader = LoadShader(0, "resources/shaders/blur.fs");
     SetShaderValue(blurrShader, GetShaderLocation(blurrShader, "resolution"), &((Vector2){1200.0f, 800.0f}), SHADER_UNIFORM_VEC2);
 
     SetTargetFPS(60);
-    Map* map;
-    map = MapParser(0, map);
-    if(!map){
-        TraceLog(LOG_ERROR, "Map Loader Error");
-    }
     while (!WindowShouldClose())
     {
 
+#ifdef MAICON_TESTING
+        MaiconUpdate(GetFrameTime());
+#endif
+#ifdef KOMATR_TESTING
+        KomatrUpdate(GetFrameTime());
+#endif
 
         if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 2.0f;
         if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 2.0f;
@@ -43,13 +59,23 @@ int main(void)
             BeginShaderMode(blurrShader);
             BeginMode2D(camera);
 
-            DrawTextureV(map->tilesets[0].tileTexture, (Vector2){0}, WHITE);
-
+#ifdef MAICON_TESTING
+            MaiconDraw();
+#endif
+#ifdef KOMATR_TESTING
+            KomatrDraw();
+#endif
             DrawRectangleV(playerPosition, (Vector2){25, 25}, MAROON);
+            EndMode2D();
         EndDrawing();
     }
 
-    MapFree(map);
+#ifdef MAICON_TESTING
+    MaiconUnload();
+#endif
+#ifdef KOMATR_TESTING
+    KomatrUnload();
+#endif
 
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
