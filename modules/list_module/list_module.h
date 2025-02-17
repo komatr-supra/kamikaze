edc0b4af7cfe65950d625696be5ed88a30bd42b8
komatr <rudolf.bouzek@seznam.cz>
2025-02-17 06:26:56 +0100

    list collection

--- modules/list_module/list_module.h
+++ modules/list_module/list_module.h
@@ -5,74 +5,69 @@
 #include "raylib.h"
 
 typedef struct ListInt{
-    int count;
-    int maxSize;
-    bool isSorted;
-    int* list;
+    int size;           // number of items in list
+    int maxSize;        // maximum number of items
+    bool isSorted;      // keep sorted this list?
+    int* collection;    // values
 } ListInt;
 
-static ListInt* ListIntCreate(int size, bool isSorted)
+static ListInt ListIntCreate(int size)
 {
-    void* allocatedMemory = malloc(sizeof(ListInt) + sizeof(int) * size);
-    ListInt* list = (ListInt*)allocatedMemory;
-    list->count = 0;
-    list->maxSize = size;
-    list->isSorted = isSorted;
-    list->list = (int*)(list + 1);
+    int* allocatedMemory = (int*)calloc(sizeof(int), sizeof(int) * size);
+    ListInt list;
+    list.size = 0;
+    list.maxSize = allocatedMemory == NULL ? 0 : size;
+    list.collection = allocatedMemory;
     return list;
 }
 
 static void ListIntAdd(ListInt* list, int value)
 {
-    if(list->count + 1 >= list->maxSize)
+    if(list->size + 1 >= list->maxSize)
     {
         TraceLog(LOG_DEBUG, "too much item in list");
         return;
     }
     if(!list->isSorted)
     {
-        list->list[list->count] = value;
-        list->count++;
+        //list is not sorted, just add an element
+        list->collection[list->size] = value;
+        list->size++;
     }
     else
     {
-        int index = list->count - 1;
-        while (index > 0 && list->list[index] > value)
+        //list is sorted, check from end
+        int index = list->size - 1;
+        while (index > 0 && list->collection[index] > value)
         {
-            list->list[index + 1] = list->list[index];
+            list->collection[index + 1] = list->collection[index];
             index--;
         }
-        list->count++;
+        list->size++;
         index++;
-        list->list[index] = value;
+        list->collection[index] = value;
     }
 }
 
 static void ListIntRemove(ListInt* list, int index)
 {
-    if(index >= 0 && index < list->count)
+    if(index >= 0 && index < list->size)
     {
         TraceLog(LOG_DEBUG, "given index dont exist");
         return;
     }
     if(!list->isSorted)
     {
-        list->list[index] = list->list[list->count - 1];
+        list->collection[index] = list->collection[list->size - 1];
     }
     else
     {
-        for (int i = index; i < list->count - 1; i++)
+        for (int i = index; i < list->size - 1; i++)
         {
-            list->list[i] = list->list[i + 1];
+            list->collection[i] = list->collection[i + 1];
         }
     }
-    list->count--;
+    list->size--;
 }
-
-typedef struct ListFree{
-    int count;
-    int maxSize;
-    void* collection;
-} ListFree;
 
 #endif
